c1222fcccd7b866ae0dab44c0a562c3972780e7e
Frédérick Van Isschot <46893914+Fred-Vaniss@users.noreply.github.com>
2019-06-06 12:05:04 +0200

    Affichage des images dans la page HTML

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# json
-Test de lecture d'un json en Javascript
+# Gallerie en AJAX local
+Génération d'une gallerie en JavaScript à l'aide un fichier JSON lu avec l'AJAX
--- main.js
+++ main.js
@@ -1,30 +1,51 @@
-console.log("working")
-var requestURL = 'https://raw.githubusercontent.com/Fred-Vaniss/json/master/data.json';
+var requestURL = 'data.json';
 
-var request = new XMLHttpRequest;
+var req = new XMLHttpRequest;
 
-request.open('GET', requestURL);
+req.open('GET', requestURL);
 
-request.responseType = 'json';
-request.send();
+req.responseType = 'json';
+req.send();
 
-request.onload = function() {
-    var gallery = request.response;
-    populateHeader(gallery);
-    // showHeroes(gallery);
+let gallery;
+let zonGal = document.getElementById("zoneGal")
+let zonImg = document.getElementById("zoneImg")
 
+req.onreadystatechange = () => {
+    if(req.readyState === XMLHttpRequest.DONE){
+        if(req.status == 200){
+            gallery = req.response;
+            console.table(gallery);
+            
+            generateGal(gallery);
+        } else {
+            console.error(`Erreur ${req.status} lors de la requete AJAX`)
+            let errorText = document.createElement("h2")
+            errorText.innerHTML = `Erreur ${req.status} lors de la requete AJAX`
+            errorText.style.color = `red`
+            zonGal.appendChild(errorText)
+        }
+    }
 }
 
-// function generateGal(jsonObj){
-//     for (let i = 0; i < jsonObj.length; i++) {
-//         zonGal.innerHTML +=  `
-//             <a href="#" class="imgElem" id="${i}">
-//                 <img src="${jsonObj[i].thumb}" alt="${jsonObj[i].desc}">
-//             </a>
-//         `
-//     }
+function generateGal(jsonObj){
+    for (let i = 0; i < jsonObj.length; i++) {
+        zonGal.innerHTML +=  `
+            <a href="#" class="imgElem" id="${i}">
+                <img src="${jsonObj[i].thumb}" alt="${jsonObj[i].desc}">
+            </a>
+        `
+    }
 
-//     for (let i = 0; i < jsonObj.length; i++) {
-//         document.getElementsByClassName("imgElem")[i].addEventListener("click", showImg)
-//     }
-// }
+    for (let i = 0; i < jsonObj.length; i++) {
+        document.getElementsByClassName("imgElem")[i].addEventListener("click", showImg)
+    }
+}
+
+function showImg(event) {
+    let i = this.id
+    zonImg.innerHTML = `
+        <h2>${gallery[i].desc}</h2><br>
+        <img src="${gallery[i].img}" alt="${gallery[i].desc}">
+    `
+}
